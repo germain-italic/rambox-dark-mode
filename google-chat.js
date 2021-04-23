b28ec8e1342d77b43059758f99b1e4d303d3c8b6
Germain Metti <Germain-Italic@users.noreply.github.com>
2021-04-23 03:00:12 +0200

    Update google-chat.js

--- google-chat.js
+++ google-chat.js
@@ -1,11 +1,3 @@
-(()=>{ let css=`
-* {
-    color: black;
-}
 .Bl2pUd, .QTQg5e, .mfLLkf, .McQwEc, .oGsu4, .XbbXmb.zzVqCe .nF6pT, .dsoUjb .jGyvbd .jO0Dzb {
     background-color: grey;
 }
-`;
-var ss = document.createElement("style");
-ss.type = "text/css"; ss.innerHTML = css;
-document.getElementsByTagName("head")[0].appendChild(ss); })();
