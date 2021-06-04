95d74df19ff3b0a2e897b672df5fd930c970af76
Germain <germain@italic.fr>
2021-06-04 15:28:36 +0200

    test darkreader

--- google-chat/custom.js
+++ google-chat/custom.js
@@ -1,16 +1,7 @@
 (()=>{
-    fetch('https://cdn.jsdelivr.net/npm/darkreader@4.9.32/darkreader.min.js')
-    .then(res => res.text())
-    .then(js => {
-        var jsElement = document.createElement("script");
-        jsElement.type = "application/javascript";
-        jsElement.src = script;
-        document.body.appendChild(js);
-
-        DarkReader.auto({
-            brightness: 100,
-            contrast: 90,
-            sepia: 10
-        });
-    });
+    const newScript = document.createElement('script');
+    newScript.src = "https://cdn.jsdelivr.net/npm/darkreader@4.9.32/darkreader.min.js";
+    document.body.appendChild(newScript);
+    DarkReader.setFetchMethod(window.fetch)
+    DarkReader.enable();
 })();
